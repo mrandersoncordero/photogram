2ad7db879ad7c9372e9b07891f44780a7daeeb27
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-04 15:44:38 -0400

    agregar ProfileAdmin y personalizar

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,31 @@
+"""Admin module."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from users.models import Profile
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile admin."""
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk','user')
+    list_editable = ('phone_number', 'website')
+
+    search_fields = (
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+    )
+
+    
