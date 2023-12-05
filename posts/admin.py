acdb50b48e950593cf63d45014751e8831de167c
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-05 09:02:14 -0400

    PostAdmin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,43 @@
+"""Admin Posts module."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+
+    list_display = ('pk', 'user', 'title', 'photo')
+    list_editable = ('title', 'photo')
+
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'title',
+    )
+
+    list_filter = (
+        'created',
+        'modified',
+    )
+
+    fieldsets = (
+        ('Data post', {
+            'fields': (
+                'title', 'photo'
+            ),
+        }),
+        ('Metadata', {
+            'fields': (
+                ('created', 'modified'),
+                ('user', 'profile')
+            ),
+        }),
+    )
+
+    readonly_fields = ('created', 'modified', 'user')
