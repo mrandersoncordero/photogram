cc917013cd934d2904c5c14a0497ce6e007e0210
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-05 08:51:52 -0400

    modificar url para servir los archivos estaticos

--- photogram/urls.py
+++ photogram/urls.py
@@ -3,8 +3,8 @@
 # Django
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
+from django.conf import settings
+from django.conf.urls.static import static
 # app
 from photogram import views as local_views
 from posts import views as posts_views
@@ -18,4 +18,4 @@ urlpatterns = [
 
 
     path('posts', posts_views.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
