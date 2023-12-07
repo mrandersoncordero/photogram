75276f98bf9c42d5cfb21799debfde5441c8463e
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-07 11:02:31 -0400

    LOGIN_URL

--- photogram/settings.py
+++ photogram/settings.py
@@ -137,6 +137,8 @@ STATICFILES_FINDERS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = 'media/'
+
+LOGIN_URL = 'users/login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
