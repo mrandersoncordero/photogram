e3495cce9bf6f94450b27cd612ff37c58457767f
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-07 11:02:42 -0400

    namespaces de las rutas

--- photogram/urls.py
+++ photogram/urls.py
@@ -5,17 +5,20 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+
 # app
 from photogram import views as local_views
 from posts import views as posts_views
-
+from users import views as users_views
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
-    path('hello-world', local_views.hello),
-    path('sorted', local_views.sorted_integers),
-    path('hi/<str:name>/<int:age>', local_views.say_hi),
+    path('hello-world', local_views.hello, name='hello_world'),
+    path('sorted', local_views.sorted_integers, name='sort'),
+    path('hi/<str:name>/<int:age>', local_views.say_hi, name='hi'),
 
 
-    path('posts', posts_views.list_posts),
+    path('posts', posts_views.list_posts, name='feed'),
+
+    path('users/login/', users_views.login_view, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
