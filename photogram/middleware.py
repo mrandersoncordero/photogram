133ead963c0088c2018dc43d94e94f97253339e1
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-07 16:19:43 -0400

    crear middleware

---
+++ photogram/middleware.py
@@ -0,0 +1,32 @@
+"""Photogram middleware catalog."""
+
+# Django
+from django.shortcuts import redirect
+from django.urls import reverse
+
+class ProfileCompletionMiddleware:
+    """Profile completion middleware.
+    
+    Ensure every that is interacting with the platform
+    have their profile picture and biography
+    """
+
+    def __init__(self, get_response):
+        """Middleware initialization."""
+        self.get_response = get_response
+    
+
+    def __call__(self, request):
+        """Code to be executed for each request before the view is called."""
+        if not request.user.is_anonymous:
+            profile = request.user.profile
+            if not profile.picture or not profile.biography:
+                # Si el usuario no tiene un picture o biography
+                if request.path not in [reverse('update_profile'), reverse('logout')]:
+                    """Redireccionar
+                    
+                    Solo si la url es diferente a [update_profile y logout]."""
+                    return redirect('update_profile')
+            
+        response = self.get_response(request)
+        return response
