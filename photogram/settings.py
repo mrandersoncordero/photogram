f59bfbc61314d6971c14a26ca7866620c8a158bf
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-07 16:19:57 -0400

    utilizar middleware

--- photogram/settings.py
+++ photogram/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'photogram.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'photogram.urls'
@@ -138,7 +139,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = 'media/'
 
-LOGIN_URL = 'users/login'
+LOGIN_URL = '/users/login/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
