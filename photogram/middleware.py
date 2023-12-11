df83a06a0b10d4c62dd09bf8472cd15e63b94b28
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-11 08:27:22 -0400

    condicion para el admin

--- photogram/middleware.py
+++ photogram/middleware.py
@@ -19,14 +19,15 @@ class ProfileCompletionMiddleware:
     def __call__(self, request):
         """Code to be executed for each request before the view is called."""
         if not request.user.is_anonymous:
-            profile = request.user.profile
-            if not profile.picture or not profile.biography:
-                # Si el usuario no tiene un picture o biography
-                if request.path not in [reverse('update_profile'), reverse('logout')]:
-                    """Redireccionar
-                    
-                    Solo si la url es diferente a [update_profile y logout]."""
-                    return redirect('update_profile')
+            if not request.user.is_staff:
+                profile = request.user.profile
+                if not profile.picture or not profile.biography:
+                    # Si el usuario no tiene un picture o biography
+                    if request.path not in [reverse('update_profile'), reverse('logout')]:
+                        """Redireccionar
+                        
+                        Solo si la url es diferente a [update_profile y logout]."""
+                        return redirect('update_profile')
             
         response = self.get_response(request)
         return response
