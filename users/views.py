4343bce58c8faa638f7e08716c484863d8dde732
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-11 09:11:27 -0400

    modificar vista de update_profile

--- users/views.py
+++ users/views.py
@@ -7,14 +7,39 @@ from django.shortcuts import render, redirect
 
 # Exceptions
 from django.db.utils import IntegrityError
+
 # Models
 from django.contrib.auth.models import User
 from users.models import Profile
 
+# Forms
+from users.forms import ProfileForm
+
 @login_required
 def update_profile(request):
     """Update a user's profile view."""
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
+    
+    return render(request, 'users/update_profile.html', context={
+        'profile': profile,
+        'user': request.user,
+        'form': form
+    })
 
 
 def signup(request):
