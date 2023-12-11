b5dba1d8caf4cc79d975c30c5701c0d74c1250cd
mrandersoncordero <acorderofigueroa7@gmail.com>
2023-12-11 14:01:43 -0400

    incluir las urls de los demas archivos

--- photogram/urls.py
+++ photogram/urls.py
@@ -2,24 +2,15 @@
 
 # Django
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# app
-from photogram import views as local_views
-from posts import views as posts_views
-from users import views as users_views
 urlpatterns = [
     # admin
     path('admin/', admin.site.urls),
 
-    path('', posts_views.list_posts, name='feed'),
-    path('posts/new', posts_views.create_post, name='create_post'),
-
-    path('users/login/', users_views.login_view, name='login'),
-    path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup, name='signup'),
-    path('users/me/profile', users_views.update_profile, name='update_profile'),
-
+    path('', include(('posts.urls', 'posts'), namespace='posts')),  # Corregir aquí
+    path('users/', include(('users.urls', 'users'), namespace='users')),  # Corregir aquí
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
